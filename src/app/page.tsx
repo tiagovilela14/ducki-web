82fdc02b46414925d192fa06e4752dd0655b6e05
tiagovilela14 <tiagovilela14@gmail.com>
2025-11-23 22:25:27 +0100

    chore: initial Ducki setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-semibold">Hello Ducki 🦆</h1>
+    </main>
+  );
+}
